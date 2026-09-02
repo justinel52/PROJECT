2fcf3f9b6002769e9729814a50c93e8f7664b9f4
justine malakas <justinelapena52@gmail.com>
2026-09-02 22:59:59 +0800

    wdawdawad

---
+++ Assignment3.java
@@ -0,0 +1,40 @@
+public class Assignment3 {
+    public static void main(String[] args) {
+
+        int Age = 17;
+        // int variable will be used for the Age number
+
+        double GWA = 96.0;
+        // double will be used for the GWA because it can contain decimal values
+
+        String Contact = "09612163044";
+        // String will be used for the contact number
+
+        String First = "Dennis";
+        // String will be used for the first name
+
+        String Last = "Lapena";
+        // String will be used for the last name
+
+        String Address = "Mabanogbog";
+        // String will be used for the address
+
+        System.out.println("First Name: " + First);
+        // This line is used to print the First Name
+
+        System.out.println("Last Name: " + Last);
+        // This line is used to print the Last Name
+
+        System.out.println("Address: " + Address);
+        // This line is used to print the Address
+
+        System.out.println("Contact Number: " + Contact);
+        // This line is used to print the Contact Number
+
+        System.out.println("Age: " + Age);
+        // This line is used to print the Age
+
+        System.out.println("GWA: " + GWA);
+        // This line is used to print the GWA
+    }
+}
